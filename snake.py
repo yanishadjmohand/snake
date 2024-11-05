cddfca4541670a1968bff88eda3371770f6bede0
Yanis Hadj_Mohand <yanis.hadj_mohand@etu.minesparis.psl.eu>
2024-11-05 14:59:37 +0100

    blblbl3

--- snake.py
+++ snake.py
@@ -1,42 +1,9 @@
-"""
-v1 : pareil mais au moins on peut sortir du programme
-avec la touche 'q', ou avec la souris en fermant la fenêtre
-"""
-
-from random import randint
-import pygame as pg
-
-pg.init()
-screen = pg.display.set_mode((400, 300))
-clock = pg.time.Clock()
-
-# on rajoute une condition à la boucle: si on la passe à False le programme s'arrête
-running = True
-while running:
-
-    clock.tick(1)
-
-    # on itère sur tous les évênements qui ont eu lieu depuis le précédent appel
-    # ici donc tous les évènements survenus durant la seconde précédente
-    for event in pg.event.get():
-        # chaque évênement à un type qui décrit la nature de l'évênement
-        # un type de pg.QUIT signifie que l'on a cliqué sur la "croix" de la fenêtre
-        if event.type == pg.QUIT:
-            running = False
-        # un type de pg.KEYDOWN signifie que l'on a appuyé une touche du clavier
-        elif event.type == pg.KEYDOWN:
-            # si la touche est "Q" on veut quitter le programme
-            if event.key == pg.K_q:
-                running = False
-
-    # xxx ici c'est discutable, car si on tape 'q'
-    # on va quand même changer de couleur avant de sortir...
-
-    random_color = (randint(0, 255), randint(0, 255), randint(0, 255))
-    screen.fill(random_color)
-    pg.display.update()
-
-
-# Enfin on rajoute un appel à pg.quit()
-# Cet appel va permettre à Pygame de "bien s'éteindre" et éviter des bugs sous Windows
-pg.quit()
+# les coordonnées de rectangle que l'on dessine
+x = 100 # coordonnée x (colonnes) en pixels
+y = 100 # coordonnée y (lignes) en pixels
+width = 30 # largeur du rectangle en pixels
+height = 30 # hauteur du rectangle en pixels
+rect = pg.Rect(x, y, width, height)
+# appel à la méthode draw.rect()
+color = (255, 0, 0) # couleur rouge
+pg.draw.rect(screen, color, rect)
