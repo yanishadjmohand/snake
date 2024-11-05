ce8162839911014e9a7407c25fed671d9e86fb98
Yanis Hadj_Mohand <yanis.hadj_mohand@etu.minesparis.psl.eu>
2024-11-05 15:11:30 +0100

    blblbl6

--- snake.py
+++ snake.py
@@ -1,12 +1,59 @@
+"""
+v1 : pareil mais au moins on peut sortir du programme
+avec la touche 'q', ou avec la souris en fermant la fenêtre
+"""
+
+from random import randint
 import pygame as pg
 
-screen = pg.display.set_mode((30, 30))
-# les coordonnées de rectangle que l'on dessine
-x = 100 # coordonnée x (colonnes) en pixels
-y = 100 # coordonnée y (lignes) en pixels
-width = 30 # largeur du rectangle en pixels
-height = 30 # hauteur du rectangle en pixels
-rect = pg.Rect(x, y, width, height)
-# appel à la méthode draw.rect()
-color = (255, 0, 0) # couleur rouge
-pg.draw.rect(screen, color, rect)
+pg.init()
+screen = pg.display.set_mode((400, 300))
+clock = pg.time.Clock()
+
+# on rajoute une condition à la boucle: si on la passe à False le programme s'arrête
+running = True
+while running:
+
+    clock.tick(1)
+
+    # on itère sur tous les évênements qui ont eu lieu depuis le précédent appel
+    # ici donc tous les évènements survenus durant la seconde précédente
+    for event in pg.event.get():
+        # chaque évênement à un type qui décrit la nature de l'évênement
+        # un type de pg.QUIT signifie que l'on a cliqué sur la "croix" de la fenêtre
+        if event.type == pg.QUIT:
+            running = False
+        # un type de pg.KEYDOWN signifie que l'on a appuyé une touche du clavier
+        elif event.type == pg.KEYDOWN:
+            # si la touche est "Q" on veut quitter le programme
+            if event.key == pg.K_q:
+                running = False
+
+    # xxx ici c'est discutable, car si on tape 'q'
+    # on va quand même changer de couleur avant de sortir...
+    screen.fill((0,0,0))
+
+    for i in range (30):
+        for j in range (30):
+            if (i+j)%2==0 : 
+                x=i
+                y=j
+                width=1
+                height=1
+                rect=pg.Rect(x, y, width, height)
+                pg.draw.rect(screen, (255,0,0), rect)
+
+
+
+
+
+
+
+    screen.fill((0,0,0))
+    pg.display.update()
+
+
+# Enfin on rajoute un appel à pg.quit()
+# Cet appel va permettre à Pygame de "bien s'éteindre" et éviter des bugs sous Windows
+pg.quit()
+
